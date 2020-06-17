6e5db0ad58956391b843905723a7e4bbbd697ec8
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-06-17 08:15:03 +0700

    [Bài tập] Áp dụng Spring Security trong thực tế có CSDL

---
+++ src/main/java/com/codegym/services/UserDetailsServiceImpl.java
@@ -0,0 +1,53 @@
+package com.codegym.services;
+
+import com.codegym.models.AppUser;
+import com.codegym.repositories.AppRoleDAO;
+import com.codegym.repositories.AppUserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private AppUserDAO appUserDAO;
+
+    @Autowired
+    private AppRoleDAO appRoleDAO;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = this.appUserDAO.findUserAccount(username);
+
+        if (appUser == null) {
+            System.out.println("User not found! " + username);
+            throw new UsernameNotFoundException("User " + username + " was not found in the database");
+        } else {
+            System.out.println("Found User: " + appUser);
+        }
+//
+        List<String> roleNames = this.appRoleDAO.getRoleNames(appUser.getUserId());
+
+        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+        if (roleNames != null) {
+            for (String role : roleNames) {
+                GrantedAuthority authority = new SimpleGrantedAuthority(role);
+                grantList.add(authority);
+            }
+        }
+
+        UserDetails userDetails = (UserDetails) new User(appUser.getUserName(), appUser.getEncryptedPassword(), grantList);
+
+        return userDetails;
+    }
+}
